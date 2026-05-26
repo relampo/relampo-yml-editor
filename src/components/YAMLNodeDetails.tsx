b9cf84a3da60c965c530b6c1b744207a27339c4f
David Cartaya <dcartaya@skillmil.com>
2026-05-26 19:54:32 -0300

    Temporarily disable add-child actions across all node types
    Hides add-child controls for all node types by commenting out the AddChildActions component to prevent invalid child additions until the feature is ready for re-enablement.

--- src/components/YAMLNodeDetails.tsx
+++ src/components/YAMLNodeDetails.tsx
@@ -120,7 +120,9 @@ export function YAMLNodeDetails({
           </div>
         )}
 
-        {addableItems.length > 0 && onAddChildNode && !isRequestNode && node.type !== 'parallel' && node.type !== 'group' && node.type !== 'if' && (
+        {/* TODO: Enable this when ready */}
+
+        {/* {addableItems.length > 0 && onAddChildNode && !isRequestNode && node.type !== 'parallel' && node.type !== 'group' && node.type !== 'if' && (
           <AddChildActions
             nodeId={node.id}
             parentNodeType={node.type}
@@ -130,7 +132,7 @@ export function YAMLNodeDetails({
             title={t('yamlEditor.common.add')}
             compact={isCompactDetailsNode}
           />
-        )}
+        )} */}
 
         <NodeDetailsContent
           node={node}
